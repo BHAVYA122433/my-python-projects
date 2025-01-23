a1b20870249c275a2fdf9b16590f33c1a0599b65
BHAVYA122433 <bhavyatayal758129@gamil.com>
2025-01-23 08:04:59 +0530

    completed my guess.py code

--- THE PERFECT GUESS.py
+++ THE PERFECT GUESS.py
@@ -1,1 +1,22 @@
+import random
+
+n = random.randint(0, 100)
+print('You are in a game')
+user_guess = None
+i = 1
+
+while user_guess != n:
+    user_guess = int(input('Please enter your guess --> '))
+    
+    if user_guess > n:
+        print('Enter a lower number')
+        i += 1
+        
+    elif user_guess < n:
+        print('Enter a higher number')
+        i += 1
+    
+    else: 
+        print('You guessed right!')
+        print(f'You took a total of {i} guesses')
 
